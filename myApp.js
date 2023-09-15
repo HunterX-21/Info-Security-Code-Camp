b6d81a5d1e8bf8c92ea789aa6f75c1655772b1b9
david_eo <david_eo@energenious.eu>
2023-09-15 11:23:40 +0000

    First main commit

--- myApp.js
+++ myApp.js
@@ -1,35 +1,7 @@
 const express = require('express');
 const app = express();
 const helmet = require('helmet');
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`😊 Server is running on server ${PORT}`)});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const path = require('path');
 
 
 
@@ -57,10 +29,10 @@ const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
-app.get("/", function (request, response) {
+app.get("/", (request, response)=> {
   response.sendFile(__dirname + '/views/index.html');
 });
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+  console.log(`😊 Your app is listening on port ${port}`);
 });
