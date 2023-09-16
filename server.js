672e3c8f0c8b9b06879fb2475e83d54e14229c21
david_eo <david_eo@energenious.eu>
2023-09-16 12:59:53 +0000

    rain

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 app.disable("x-powered-by");
 var fs = require("fs");
 var path = require("path");
-var bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
