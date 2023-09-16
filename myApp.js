c66f1497939db55c80a599fb17446b8846066860
david_eo <david_eo@energenious.eu>
2023-09-16 10:48:39 +0000

    Eight commit

--- myApp.js
+++ myApp.js
@@ -12,7 +12,7 @@ app.use(helmet.ieNoOpen());
 const ninetyDaysInSeconds = 90*24*60*60*1000;
 app.use(helmet.hsts({maxAge:ninetyDaysInSeconds, force: true}));
 
-
+app.use(helmet.dnsPrefetchControl());
 
 
 
