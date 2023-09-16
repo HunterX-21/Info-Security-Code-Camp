7e25cd3d671c8d07e7162a5c1b7caeb04e90ee6f
david_eo <david_eo@energenious.eu>
2023-09-16 11:10:04 +0000

    Eleventh main commit

--- myApp.js
+++ myApp.js
@@ -3,18 +3,32 @@ const app = express();
 const helmet = require('helmet');
 const path = require('path');
 
-app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({action: 'deny'}));
-app.use(helmet.xssFilter());
-app.use(helmet.noSniff());
-app.use(helmet.ieNoOpen());
+
+app.use(helmet({
+  frameguard: {         // configure
+    action: 'deny'
+  },
+  contentSecurityPolicy: {    // enable and configure
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ['style.com'],
+    }
+  },
+  dnsPrefetchControl: false     // disable
+}));
+
+// app.use(helmet.hidePoweredBy());
+// app.use(helmet.frameguard({action: 'deny'}));
+// app.use(helmet.xssFilter());
+// app.use(helmet.noSniff());
+// app.use(helmet.ieNoOpen());
 
 const ninetyDaysInSeconds = 90*24*60*60*1000;
 app.use(helmet.hsts({maxAge:ninetyDaysInSeconds, force: true}));
 
-app.use(helmet.dnsPrefetchControl());
-app.use(helmet.noCache());
-app.use(helmet.contentSecurityPolicy({ directives: { defaultSrc: ["'self'"], scriptSrc: ["'self'", "trusted-cdn.com"] }} ));
+//app.use(helmet.dnsPrefetchControl());
+//app.use(helmet.noCache());
+//app.use(helmet.contentSecurityPolicy({ directives: { defaultSrc: ["'self'"], scriptSrc: ["'self'", "trusted-cdn.com"] }} ));
 
 
 
